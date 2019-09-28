9d9d77ec05bc279fa1262b6fb2425bc4a054ce4d
ntsk <ridha.h2a26@gmail.com>
2019-09-29 00:53:45 +0900

    bundle gem deploygate-client

---
+++ lib/deploygate/client.rb
@@ -0,0 +1,8 @@
+require "deploygate/client/version"
+
+module Deploygate
+  module Client
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/deploygate/client_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Deploygate::Client do
+  it "has a version number" do
+    expect(Deploygate::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
