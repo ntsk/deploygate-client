1a221066351e53b6b2c971f5afe78ddde7505196
ntsk <ridha.h2a26@gmail.com>
2019-11-04 20:08:44 +0900

    Add options

--- lib/deploygate/client.rb
+++ lib/deploygate/client.rb
@@ -9,5 +9,13 @@ module Deploygate
     include Deploygate::Client::API
     include Deploygate::Client::Apps
     include Deploygate::Client::Organizations
+
+    attr_reader :symbolize_response, :open_timeout, :timeout
+
+    def initialize(options = {})
+      @symbolize_response = options[:symbolize_response] || false
+      @open_timeout = options[:open_timeout] || 10
+      @timeout = options[:timeout] || 30
+    end
   end
 end
--- lib/deploygate/client/api.rb
+++ lib/deploygate/client/api.rb
@@ -14,10 +14,12 @@ module Deploygate
 
       def api
         @api ||= Faraday.new(url: BASE_URL) do |builder|
+          builder.options.open_timeout = open_timeout
+          builder.options.timeout = timeout
           builder.request :multipart
+          builder.response :json, parser_options: { symbolize_names: true } if symbolize_response
           builder.headers['Authorization'] = "token #{token}"
           builder.adapter Faraday::default_adapter
-          #builder.response :json, parser_options: { symbolize_names: true }
         end
       end
     end
