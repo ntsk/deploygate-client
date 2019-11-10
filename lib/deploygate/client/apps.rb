72f86ca59ee44a0df91deb4fe85f297b145f08c5
ntsk <ridha.h2a26@gmail.com>
2019-11-10 22:53:43 +0900

    Add params comments

--- lib/deploygate/client/apps.rb
+++ lib/deploygate/client/apps.rb
@@ -8,6 +8,18 @@ module Deploygate
       ROLE_DEVELOPER = 1
       ROLE_TESTER = 2
 
+      # Upload an application binary file.
+      # @see https://docs.deploygate.com/reference#upload
+      #
+      # @param owner [String] User or organization name
+      # @param file_path [String] Application binary file (ipa/apk)
+      # @option message [String] Description of the file
+      # @option dist_key [String] Distribution page hash
+      # @option dist_name [String] Distribution page name
+      # @option release_note [String] Message when updating app on distribution page
+      # @option disable_notify [Boolean] Email notification when pushing (only ios)
+      # @option visibility [String] Application privacy settings (private/public)
+      # @return [Deploygate::Client::Response]
       def upload_app(owner:, file_path:, message: nil, dist_key: nil, dist_name: nil, release_note: nil, disable_notify: nil, visibility: nil)
         res = api.post "/api/users/#{owner}/apps" do |request|
           request.body = {
@@ -23,6 +35,15 @@ module Deploygate
         Response.new(res)
       end
 
+      # Invite users to the app.
+      # @see https://docs.deploygate.com/reference#invite
+      #
+      # @param owner [String] User or organization name
+      # @param platform [String] Application platform (ios/android)
+      # @param app_id [String] Application package name
+      # @param users [String] User name or email (You can specify multiple values separated by commas)
+      # @option role[String] 1(developer) or 2(tester)
+      # @return [Deploygate::Client::Response]
       def add_app_members(owner:, platform:, app_id:, users:, role: nil)
         res = api.post "/api/users/#{owner}/platforms/#{platform}/apps/#{app_id}/members" do |request|
           request.headers['Content-Type'] = 'application/x-www-form-urlencoded'
@@ -36,11 +57,26 @@ module Deploygate
         Response.new(res)
       end
 
+      # Get a list of members participating in the app.
+      # @see https://docs.deploygate.com/reference#apps-members-index
+      #
+      # @param owner [String] User or organization name
+      # @param platform [String] Application platform (ios/android)
+      # @param app_id [String] Application package name
+      # @return [Deploygate::Client::Response]
       def app_members(owner:, platform:, app_id:)
         res = api.get "/api/users/#{owner}/platforms/#{platform}/apps/#{app_id}/members"
         Response.new(res)
       end
 
+      # Delete members from the app.
+      # @see https://docs.deploygate.com/reference#apps-members-destroy
+      #
+      # @param owner [String] User or organization name
+      # @param platform [String] Application platform (ios/android)
+      # @param app_id [String] Application package name
+      # @param users [String] User name or email (You can specify multiple values separated by commas)
+      # @return [Deploygate::Client::Response]
       def delete_app_members(owner:, platform:, app_id:, users:)
         res = api.delete "/api/users/#{owner}/platforms/#{platform}/apps/#{app_id}/members" do |request|
           request.headers['Content-Type'] = 'application/x-www-form-urlencoded'
@@ -53,28 +89,59 @@ module Deploygate
         Response.new(res)
       end
 
+      # Get a list of teams participating in the app.
+      # @see https://docs.deploygate.com/reference#apps-teams-index
+      #
+      # @param org_name [String] Organization name
+      # @param platform [String] Application platform (ios/android)
+      # @param app_id [String] Application package name
+      # @return [Deploygate::Client::Response]
       def app_teams(org_name:, platform:, app_id:)
         res = api.get "/api/organizations/#{org_name}/platforms/#{platform}/apps/#{app_id}/teams"
         Response.new(res)
       end
 
-      def add_app_team(org_name:, platform:, app_id:, team:)
+      # Add teams to the app,
+      # @see https://docs.deploygate.com/reference#apps-teams-create
+      #
+      # @param org_name [String] Organization name
+      # @param platform [String] Application platform (ios/android)
+      # @param app_id [String] Application package name
+      # @param team [String] Team name
+      # @return [Deploygate::Client::Response]
+      def add_app_team(org_name:, platform:, app_id:, team_name:)
         res = api.post "/api/organizations/#{org_name}/platforms/#{platform}/apps/#{app_id}/teams" do |request|
           request.headers['Content-Type'] = 'application/x-www-form-urlencoded'
           request.body = URI.encode_www_form(
             {
-              team: team
+              team: team_name
             }
           )
         end
         Response.new(res)
       end
 
-      def delete_app_team(org_name:, platform:, app_id:, team:)
-        res = api.delete "/api/organizations/#{org_name}/platforms/#{platform}/apps/#{app_id}/teams/#{team}"
+      # Delete team from the app.
+      # @see https://docs.deploygate.com/reference#apps-teams-destroy
+      #
+      # @param org_name [String] Organization name
+      # @param platform [String] Application platform (ios/android)
+      # @param app_id [String] Application package name
+      # @param team [String] Team name
+      # @return [Deploygate::Client::Response]
+      def delete_app_team(org_name:, platform:, app_id:, team_name:)
+        res = api.delete "/api/organizations/#{org_name}/platforms/#{platform}/apps/#{app_id}/teams/#{team_name}"
         Response.new(res)
       end
 
+      # Delete distribution page.
+      # @see https://docs.deploygate.com/reference#delete-distribution-page-by-name
+      #
+      # @param owner [String] User or organization name
+      # @param platform [String] Application platform (ios/android)
+      # @param app_id [String] Application package name
+      # @param dist_name [String] Distribution page name
+      # @return [Deploygate::Client::Response]
       def delete_app_distribution(owner:, platform:, app_id:, dist_name:)
         res = api.delete "/api/users/#{owner}/platforms/#{platform}/apps/#{app_id}/distributions" do |request|
           request.headers['Content-Type'] = 'application/x-www-form-urlencoded'
--- lib/deploygate/client/organizations.rb
+++ lib/deploygate/client/organizations.rb
@@ -4,11 +4,21 @@ module Deploygate
   class Client
     module Organizations
 
+      # Get a list of organizations.
+      # @see https://docs.deploygate.com/reference#organizations-index
+      #
+      # @return [Deploygate::Client::Response]
       def organizations
         res = api.get '/api/organizations'
         Response.new(res)
       end
 
+      # Create an organization.
+      # @see https://docs.deploygate.com/reference#organizations-index
+      #
+      # @param org_name [String] Organization name
+      # @option description [String] Organization description
+      # @return [Deploygate::Client::Response]
       def add_organization(org_name:, description: nil)
         res = api.post '/api/organizations' do |request|
           request.params[:name] = org_name
@@ -17,11 +27,22 @@ module Deploygate
         Response.new(res)
       end
 
+      # Get organization information.
+      # @see https://docs.deploygate.com/reference#organizations-show
+      #
+      # @param org_name [String] Organization name
+      # @return [Deploygate::Client::Response]
       def organization(org_name:)
         res = api.get "/api/organizations/#{org_name}"
         Response.new(res)
       end
 
+      # Update organization information.
+      # @see https://docs.deploygate.com/reference#organizations-update
+      #
+      # @param org_name [String] Organization name
+      # @option description [String] Organization description
+      # @return [Deploygate::Client::Response]
       def update_organization(org_name:, description: nil)
         res = api.patch "/api/organizations/#{org_name}" do |request|
           request.params[:description] = description
@@ -29,16 +50,32 @@ module Deploygate
         Response.new(res)
       end
 
+      # Delete organization information.
+      # @see https://docs.deploygate.com/reference#organizations-destroy
+      #
+      # @param org_name [String] Organization name
+      # @return [Deploygate::Client::Response]
       def delete_organization(org_name:)
         res = api.delete "/api/organizations/#{org_name}"
         Response.new(res)
       end
 
+      # Get a list of organization members.
+      # @see https://docs.deploygate.com/reference#organizations-members-index
+      #
+      # @param org_name [String] Organization name
+      # @return [Deploygate::Client::Response]
       def organization_members(org_name:)
         res = api.get "/api/organizations/#{org_name}/members"
         Response.new(res)
       end
 
+      # Add members to the organization.
+      # @see https://docs.deploygate.com/reference#organizations-members-create
+      #
+      # @param org_name [String] Organization name
+      # @param user_name [String] User name
+      # @return [Deploygate::Client::Response]
       def add_organization_member(org_name:, user_name:)
         res = api.post "/api/organizations/#{org_name}/members" do |request|
           request.params[:username] = user_name
@@ -46,6 +83,12 @@ module Deploygate
         Response.new(res)
       end
 
+      # Add members to the organization.
+      # @see https://docs.deploygate.com/reference#organizations-members-create
+      #
+      # @param org_name [String] Organization name
+      # @param email [String] User email
+      # @return [Deploygate::Client::Response]
       def add_organization_member_by_email(org_name:, email:)
         res = api.post "/api/organizations/#{org_name}/members" do |request|
           request.params[:email] = email
@@ -53,6 +96,12 @@ module Deploygate
         Response.new(res)
       end
 
+      # Delete members to the organization.
+      # @see https://docs.deploygate.com/reference#organizations-members-destroy
+      #
+      # @param org_name [String] Organization name
+      # @param user_name [String] User name
+      # @return [Deploygate::Client::Response]
       def delete_organization_member(org_name:, user_name:)
         res = api.delete "/api/organizations/#{org_name}/members/#{user_name}" do |request|
           request.params[:id] = user_name
@@ -60,6 +109,12 @@ module Deploygate
         Response.new(res)
       end
 
+      # Delete members from the organization.
+      # @see https://docs.deploygate.com/reference#organizations-members-destroy
+      #
+      # @param org_name [String] Organization name
+      # @param email [String] User email
+      # @return [Deploygate::Client::Response]
       def delete_organization_member_by_email(org_name:, email:)
         res = api.delete "/api/organizations/#{org_name}/members/#{email}" do |request|
           request.params[:id] = email
@@ -67,11 +122,24 @@ module Deploygate
         Response.new(res)
       end
 
+      # Get a list of team members.
+      # @see https://docs.deploygate.com/reference#organizations-teams-users-index
+      #
+      # @param org_name [String] Organization name
+      # @param team_name [String] Team name
+      # @return [Deploygate::Client::Response]
       def team_members(org_name:, team_name:)
         res = api.get "/api/organizations/#{org_name}/teams/#{team_name}/users"
         Response.new(res)
       end
 
+      # Add members to the team.
+      # @see https://docs.deploygate.com/reference#organizations-teams-users-create
+      #
+      # @param org_name [String] Organization name
+      # @param team_name [String] Team name
+      # @param user_name [String] User name
+      # @return [Deploygate::Client::Response]
       def add_team_member(org_name:, team_name:, user_name:)
         res = api.post "/api/organizations/#{org_name}/teams/#{team_name}/users" do |request|
           request.params[:user] = user_name
@@ -79,6 +147,13 @@ module Deploygate
         Response.new(res)
       end
 
+      # Delete members from the team.
+      # @see https://docs.deploygate.com/reference#organizations-teams-users-destroy
+      #
+      # @param org_name [String] Organization name
+      # @param team_name [String] Team name
+      # @param user_name [String] User name
+      # @return [Deploygate::Client::Response]
       def delete_team_member(org_name:, team_name:, user_name:)
         res = api.delete "/api/organizations/#{org_name}/teams/#{team_name}/users/#{user_name}"
         Response.new(res)
