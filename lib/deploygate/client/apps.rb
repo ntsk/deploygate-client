2fb7cb83d5cd475b311eed28ed77522c59e95cf8
ntsk <ridha.h2a26@gmail.com>
2019-11-10 21:05:00 +0900

    Use form url encode

--- lib/deploygate/client/apps.rb
+++ lib/deploygate/client/apps.rb
@@ -25,10 +25,13 @@ module Deploygate
 
       def add_app_members(owner:, platform:, app_id:, users:, role: nil)
         res = api.post "/api/users/#{owner}/platforms/#{platform}/apps/#{app_id}/members" do |request|
-          request.body = {
-            users: users,
-            role: role
-          }
+          request.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+          request.body = URI.encode_www_form(
+            {
+              users: users,
+              role: role
+            }
+          )
         end
         Response.new(res)
       end
@@ -40,37 +43,46 @@ module Deploygate
 
       def delete_app_members(owner:, platform:, app_id:, users:)
         res = api.delete "/api/users/#{owner}/platforms/#{platform}/apps/#{app_id}/members" do |request|
-          request.body = {
-            users: users
-          }
+          request.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+          request.body = URI.encode_www_form(
+            {
+              users: users
+            }
+          )
         end
         Response.new(res)
       end
 
       def app_teams(org_name:, platform:, app_id:)
-        res = api.get "/api/users/#{owner}/platforms/#{platform}/apps/#{app_id}/teams"
+        res = api.get "/api/organizations/#{org_name}/platforms/#{platform}/apps/#{app_id}/teams"
         Response.new(res)
       end
 
       def add_app_team(org_name:, platform:, app_id:, team:)
-        res = api.post "/api/users/#{owner}/platforms/#{platform}/apps/#{app_id}/teams" do |request|
-          request.body = {
-            team: team
-          }
+        res = api.post "/api/organizations/#{org_name}/platforms/#{platform}/apps/#{app_id}/teams" do |request|
+          request.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+          request.body = URI.encode_www_form(
+            {
+              team: team
+            }
+          )
         end
         Response.new(res)
       end
 
       def delete_app_team(org_name:, platform:, app_id:, team:)
-        res = api.delete "/api/users/#{owner}/platforms/#{platform}/apps/#{app_id}/teams/#{team}"
+        res = api.delete "/api/organizations/#{org_name}/platforms/#{platform}/apps/#{app_id}/teams/#{team}"
         Response.new(res)
       end
 
       def delete_app_distribution(owner:, platform:, app_id:, dist_name:)
         res = api.delete "/api/users/#{owner}/platforms/#{platform}/apps/#{app_id}/distributions" do |request|
-          request.body = {
-            distribution_name: dist_name
-          }
+          request.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+          request.body = URI.encode_www_form(
+            {
+              distribution_name: dist_name
+            }
+          )
         end
         Response.new(res)
       end
