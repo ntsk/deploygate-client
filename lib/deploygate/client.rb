2c465a9530db6a5833ef42f2c6a25645072a9983
ntsk <ridha.h2a26@gmail.com>
2019-11-11 00:12:33 +0900

    Run rubocop with CI jobs

--- lib/deploygate/client.rb
+++ lib/deploygate/client.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'deploygate/client/version'
-require 'deploygate/client/config'
 require 'deploygate/client/api'
 require 'deploygate/client/apps'
 require 'deploygate/client/organizations'
